440b816651caaaadc259668f7d2254d838e2ed48
Aniket Gore <105165172+aniketgcs@users.noreply.github.com>
2022-09-24 22:54:24 +0530

    Add files via upload

---
+++ Practical Codes/Artificial Intelligence/P1/bfs.py
@@ -0,0 +1,53 @@
+cities=dict(
+Arad=dict(Zerind=75,Timisoara=118,Sibiu=140),
+Bucharest=dict(Urziceni=85,Giurgiu=90,Pitesti=101,Fagaras=211),
+Craiova=dict(Drobeta=120,Pitesti=138,Rimnicu=146),
+Drobeta=dict(Mehadia=75,Craiova=120),
+Eforie=dict(Hirsova=86),
+Fagaras=dict(Sibiu=99,Bucharest=211),
+Giurgiu=dict(Bucharest=90),
+Hirsova=dict(Eforie=86,Urziceni=98),
+Iasi=dict(Neamt=87,Vaslui=92),
+Lugoj=dict(Mehadia=70,Timisoara=111),
+Mehadia=dict(Lugoj=70,Drobeta=75),
+Neamt=dict(Iasi=87),
+Oradea=dict(Zerind=71,Sibiu=151),
+Pitesti=dict(Rimnicu=97,Bucharest=101,Craiova=138),
+Rimnicu=dict(Sibiu=80,Pitesti=97,Craiova=146),
+Sibiu=dict(Rimnicu=80,Fagaras=99,Arad=140,Oradea=151),
+Timisoara=dict(Lugoj=111,Arad=118),
+Urziceni=dict(Bucharest=85,Hirsova=98,Vaslui=142),
+Vaslui=dict(Iasi=92,Urziceni=142),
+Zerind=dict(Oradea=71,Arad=75)
+)
+
+import queue as Q
+
+# start=input("Enter start city: ")
+# goal=input("Enter a goal city: ")
+start="Arad"
+goal="Fagaras"
+result=''
+
+def BFS(source, cityq, visitedq):
+    global result
+    if source==start:
+        result=result+' '+source
+    for x in cities[source].keys():
+        if x==goal:
+            result=result+' '+x
+            return
+        if x not in cityq.queue and x not in visitedq.queue:
+            cityq.put(x)
+            result=result+' '+x
+    visitedq.put(source)
+    BFS(cityq.get(),cityq,visitedq)
+
+def main():
+    cityq=Q.Queue()
+    visitedq=Q.Queue()
+    BFS(start, cityq, visitedq)
+    print("BFS Traversal from ",start," to ",goal," is: ")
+    print(result)
+
+main()
